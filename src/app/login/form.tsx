d7bbf7e5fbd989243b41e91a5cc5c358a94f4dd7
Arjun Khanna <arjunkhanna0108@gmail.com>
2023-12-09 01:46:04 +0530

    feat: added email auth using firebase

--- src/app/login/form.tsx
+++ src/app/login/form.tsx
@@ -21,7 +21,7 @@ export default function LoginForm() {
 		"form-control block w-full px-4 py-5 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none";
 
 	return (
-		<form>
+		<form action="/auth/login" method="post">
 			{/* Email input field */}
 			<div className="mb-6">
 				<input
@@ -52,6 +52,15 @@ export default function LoginForm() {
 				Sign in
 			</button>
 
+			{/* Sign Up button */}
+			<button
+				type="submit"
+				className="my-4 inline-block w-full rounded bg-blue-600 px-7 py-4 text-sm font-medium uppercase leading-snug text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
+				formAction="/auth/signup"
+			>
+				Sign up
+			</button>
+
 			{/* OR divider */}
 			<div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-gray-300 after:mt-0.5 after:flex-1 after:border-t after:border-gray-300">
 				<p className="mx-4 mb-0 text-center font-semibold">OR</p>
@@ -66,26 +75,11 @@ export default function LoginForm() {
 			>
 				<img
 					className="pr-2"
-					src="/images/google.svg"
+					src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqJfKJo3DdrXnTEKARi8lVPgX06k36eXvmJWh2N731Ylo7C0tvtXd-VuueGewCQwV1QJ4&usqp=CAU"
 					alt=""
 					style={{ height: "2rem" }}
 				/>
 				Continue with Google
-			</a>
-
-			{/* Sign In with GitHub button */}
-			<a
-				className="flex w-full items-center justify-center rounded px-7 py-2 text-sm font-medium uppercase leading-snug text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
-				style={{ backgroundColor: "#000000" }}
-				role="button"
-			>
-				<img
-					className="pr-2"
-					src="/images/github.png"
-					alt=""
-					style={{ height: "2.2rem" }}
-				/>
-				Continue with GitHub
 			</a>
 		</form>
 	);
