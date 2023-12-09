1e326534b3ac38f14879ad89058ca0d2a76a5ff4
Arjun Khanna <arjunkhanna0108@gmail.com>
2023-12-09 12:56:39 +0530

    feat: added logout button on chat interface

--- src/app/chat/chatInterface.tsx
+++ src/app/chat/chatInterface.tsx
@@ -1,5 +1,111 @@
-import React from "react";
+"use client";
+import { prisma } from "@/lib/prisma";
+import { Router } from "next/router";
+import React, { useEffect, useState } from "react";
+import MessageInterface from "./messageInterface";
+import { Button } from "@/components/ui/button";
 
 export default function ChatInterface() {
-	return <div>C</div>;
+	const [chats, setChats] = useState<any[]>([]);
+	const [isLoading, setLoading] = useState(true);
+	const [selected, setSelected] = useState("");
+	var i = 0;
+
+	async function createNewChat(): Promise<void> {
+		const response = await fetch("/api/createChat", {
+			method: "POST",
+			body: JSON.stringify({}),
+		});
+		if (response.status === 200) {
+			setLoading(true);
+			// setSelected("");
+		}
+	}
+
+	useEffect(() => {
+		fetch("/api/getChats")
+			.then((response) => response.json())
+			.then((data) => {
+				setChats(data[0].data.reverse());
+				if (chats[0] !== undefined) {
+					setSelected(chats[0].id);
+				}
+				setLoading(false);
+			});
+	}, [isLoading]);
+
+	if (isLoading) return <p>Loading...</p>;
+	// if (!chats) return <p>No chats</p>;
+
+	return (
+		<div>
+			<div className="flex">
+				<div className="relative">
+					<div
+						id="sidebar"
+						className="bg-gray-700 w-[25vw] h-[90vh]  overflow-y-scroll"
+					>
+						<div id="messList" className="mt-4 mx-4 space-y-4">
+							<div
+								id="message-preview"
+								className=" text-white w-inherit h-[50px] flex justify-start items-center bg-gray-600 z-10 rounded-lg overflow-hidden"
+								onClick={createNewChat}
+							>
+								{/* <i className="fa-solid fa-square-plus"></i>  */}
+								<p
+									style={{ whiteSpace: "nowrap" }}
+									className="overflow-hidden mx-4"
+								>
+									<i className="fa-regular fa-pen-to-square"></i>{" "}
+									New Chat
+								</p>
+							</div>
+							{chats.map((element: { id: string }) => {
+								i++;
+								return (
+									<div
+										key={element.id}
+										onClick={() => setSelected(element.id)}
+										id="message-preview"
+										className={
+											` text-white w-inherit h-[50px] flex justify-start items-center hover:bg-gray-600 z-10 rounded-lg overflow-hidden ` +
+											(selected === element.id
+												? "bg-gray-600"
+												: "")
+										}
+									>
+										<p
+											style={{ whiteSpace: "nowrap" }}
+											className="overflow-hidden mx-4"
+										>
+											Chat no. {chats.length - i + 1}
+										</p>
+									</div>
+								);
+							})}
+						</div>
+					</div>
+					<div className="h-[10vh] bg-gray-700">
+						<form
+							className="absolute bottom-[2vh] flex justify-center w-full"
+							method="post"
+						>
+							<Button
+								className="bg-none"
+								id="logout"
+								formAction="/auth/logout"
+							>
+								Logout
+							</Button>
+						</form>
+					</div>
+				</div>
+				{selected !== "" ? (
+					<MessageInterface key={selected} chatID={selected} />
+				) : (
+					<div></div>
+				)}
+			</div>
+		</div>
+	);
 }
--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,95 +1,21 @@
-"use client";
-import { prisma } from "@/lib/prisma";
-import { Router } from "next/router";
-import React, { useEffect, useState } from "react";
-import MessageInterface from "./messageInterface";
+import React from "react";
+import { redirect } from "next/navigation";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import ChatInterface from "./chatInterface";
 
-export default function ChatInterface() {
-	const [chats, setChats] = useState<any[]>([]);
-	const [isLoading, setLoading] = useState(true);
-	const [selected, setSelected] = useState("");
-	var i = 0;
+export default async function ChatPage() {
+	const supabase = createServerComponentClient({ cookies });
 
-	async function createNewChat(): Promise<void> {
-		const response = await fetch("/api/createChat", {
-			method: "POST",
-			body: JSON.stringify({}),
-		});
-		if (response.status === 200) {
-			setLoading(true);
-			// setSelected("");
-		}
+	const { data } = await supabase.auth.getSession();
+
+	if (!data.session) {
+		return redirect("/login");
 	}
-
-	useEffect(() => {
-		fetch("/api/getChats")
-			.then((response) => response.json())
-			.then((data) => {
-				setChats(data[0].data.reverse());
-				if (chats[0] !== undefined) {
-					setSelected(chats[0].id);
-				}
-				setLoading(false);
-			});
-	}, [isLoading]);
-
-	if (isLoading) return <p>Loading...</p>;
-	// if (!chats) return <p>No chats</p>;
 
 	return (
 		<div>
-			<div className="flex">
-				<div
-					id="sidebar"
-					className="bg-gray-700 w-[25vw] h-[100vh] rounded-lg overflow-y-scroll"
-				>
-					<div id="messList" className="mt-4 mx-4 space-y-4">
-						<div
-							id="message-preview"
-							className=" text-white w-inherit h-[50px] flex justify-start items-center bg-gray-600 z-10 rounded-lg overflow-hidden"
-							onClick={createNewChat}
-						>
-							{/* <i className="fa-solid fa-square-plus"></i>  */}
-							<p
-								style={{ whiteSpace: "nowrap" }}
-								className="overflow-hidden mx-4"
-							>
-								<i className="fa-regular fa-pen-to-square"></i>{" "}
-								New Chat
-							</p>
-						</div>
-
-						{chats.map((element: { id: string }) => {
-							i++;
-							return (
-								<div
-									key={element.id}
-									onClick={() => setSelected(element.id)}
-									id="message-preview"
-									className={
-										` text-white w-inherit h-[50px] flex justify-start items-center hover:bg-gray-600 z-10 rounded-lg overflow-hidden ` +
-										(selected === element.id
-											? "bg-gray-600"
-											: "")
-									}
-								>
-									<p
-										style={{ whiteSpace: "nowrap" }}
-										className="overflow-hidden mx-4"
-									>
-										Chat no. {chats.length - i + 1}
-									</p>
-								</div>
-							);
-						})}
-					</div>
-				</div>
-				{selected !== "" ? (
-					<MessageInterface key={selected} chatID={selected} />
-				) : (
-					<div></div>
-				)}
-			</div>
+			<ChatInterface />
 		</div>
 	);
 }
